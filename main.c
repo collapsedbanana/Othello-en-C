46a4cda112a3c2db9c359b2e3fbba1c39c8cdbc8
collapsedbanana <78849946+collapsedbanana@users.noreply.github.com>
2023-01-13 18:54:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,64 +1,64 @@
 #include <stdio.h>
 
 /* Largeur de la grille */
-#define N 8
+#define Taille 8
 
 /* Pions du jeu */
 #define VIDE ' '
-#define NOIR 'X'  /* joueur 1 */
-#define BLANC 'O' /* joueur 2 */
+#define N 'X'  /* joueur 1 */
+#define B 'O' /* joueur 2 */
 
 /* Type du plateau de jeu */
-typedef char t_matrice[N][N];
+typedef char T_Othellier[Taille][Taille];
 
 /* Fonction pour initialiser la grille */
-void init_matrice (t_matrice m) {
+void init_matrice (T_Othellier m) {
     int i, j;
 
     /* On met tout a vide */
-    for (i=0; i<N; i++)
-        for (j=0; j<N; j++)
+    for (i=0; i<Taille; i++)
+        for (j=0; j<Taille; j++)
             m[i][j] = VIDE;
 
     /* On place les 4 premiers pions */
-    m[N/2-1][N/2-1] = NOIR;
-    m[N/2-1][N/2] = BLANC;
-    m[N/2][N/2-1] = BLANC;
-    m[N/2][N/2] = NOIR;
+    m[Taille/2-1][Taille/2-1] = N;
+    m[Taille/2-1][Taille/2] = B;
+    m[Taille/2][Taille/2-1] = B;
+    m[Taille/2][Taille/2] = N;
 }
 
 /* Fonction pour afficher la grille */
-void afficher_matrice (t_matrice m) {
+void afficher_matrice (T_Othellier m) {
     int i, j;
     char a = 'A';
 
     /* Affichage des lettres */
     printf ("\n");
-    for (i=0; i<N; i++) {
+    for (i=0; i<Taille; i++) {
         printf ("  %c ", a);
         a++;
     }
 
     /* Affichage de la grille */
     printf ("\n+");
-    for (i=0; i<N; i++)
+    for (i=0; i<Taille; i++)
         printf ("---+");
     printf ("\n");
-    for (i=0; i<N; i++) {
+    for (i=0; i<Taille; i++) {
         printf ("|");
-        for (j=0; j<N; j++)
-            if (m[i][j] == BLANC)
+        for (j=0; j<Taille; j++)
+            if (m[i][j] == B)
                 printf ("\033[31m %c \033[0m|", m[i][j]); /* Les blancs en rouge */
             else
                 printf ("\033[34m %c \033[0m|", m[i][j]); /* Les noirs en bleu */
         printf (" %d\n+", i+1);
-        for (j=0; j<N; j++)
+        for (j=0; j<Taille; j++)
             printf ("---+");
         printf ("\n");
     }
 }
 int main () {
-    t_matrice m;
+    T_Othellier m;
     int lig, col, joueur = 1;
 
     /* Initialisation du jeu */
@@ -67,4 +67,5 @@ int main () {
 
     return 0;
 }
+
 // pour choisir la couleur : https://gist.github.com/vratiu/9780109
